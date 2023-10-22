1c46c992ef3b999d7e51e60c6439e0db593922c7
Ravi Kumar <ravikumar46931@gmail.com>
2023-10-22 20:20:18 +0530

    data transformation, utils

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -7,6 +7,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 from dataclasses import dataclass
 
+from src.components.data_transformation import DataTransformation
+from src.components.data_transformation import DataTransformationConfig
+
 @dataclass
 class DataIngestionConfig:
     train_data_path: str=os.path.join('artifacts',"train.csv")
@@ -48,7 +51,11 @@ class DataIngestion:
 # To check the code is working fine or not
 if __name__=="__main__":
     obj=DataIngestion()
-    obj.initiate_data_ingestion()
+    # obj.initiate_data_ingestion()
+    train_data,test_data=obj.initiate_data_ingestion()
+
+    data_transformation=DataTransformation()
+    data_transformation.initiate_data_transformation(train_data,test_data)
 
 
 # If there is error occured in src.exception or src.logger them there should be implementation of setup.py
