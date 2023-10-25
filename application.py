ee589048ccf5ac3ec85c83dd31e25b373a53e098
Ravi Kumar <ravikumar46931@gmail.com>
2023-10-25 20:43:39 +0530

    def

--- application.py
+++ application.py
@@ -37,9 +37,5 @@ def predict_datapoint():
 
         return render_template('home.html', results= results[0])
 
-
-
-#vals
-
-if __name__=="__main__":
-    app.run(host='0.0.0.0')
+# if __name__=="__main__":
+#     app.run(host='0.0.0.0')
